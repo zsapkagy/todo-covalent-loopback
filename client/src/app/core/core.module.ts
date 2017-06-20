9a5bcf3b372f088e8320e78133b9ded608a62f16
Gyorgy Zsapka <gyorgyzsapka@Gyorgys-MacBook-Pro.local>
2017-06-20 12:53:18 +0200

    prevent the reImport of the core module

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { RouterModule } from "@angular/router";
 
 import { MainComponent } from "app/core/main/main.component";
 import { PageNotFoundComponent } from "app/core/page-not-found.component";
 import { SharedModule } from "app/shared/shared.module";
+import { throwIfAlreadyLoaded } from "app/core/module-import-guard";
 
 @NgModule({
   imports: [
@@ -19,4 +20,8 @@ import { SharedModule } from "app/shared/shared.module";
     PageNotFoundComponent,
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }
+}
