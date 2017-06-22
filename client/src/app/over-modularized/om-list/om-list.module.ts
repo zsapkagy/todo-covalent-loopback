9ebf2934226a3cf50228564adf64a9c5e7de0c7d
Gyorgy Zsapka <gyorgyzsapka@Gyorgys-MacBook-Pro.local>
2017-06-22 14:56:28 +0200

    add button to the todo list

--- client/src/app/over-modularized/om-list/om-list.module.ts
+++ client/src/app/over-modularized/om-list/om-list.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { OmListComponent } from './om-list.component';
 import { OmListItemComponent } from "app/over-modularized/om-list/om-list-item/om-list-item.component";
 import { SharedModule } from "app/shared/shared.module";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   imports: [
+    RouterModule,
     SharedModule,
   ],
   declarations: [
